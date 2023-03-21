d12e423975b087848cfe90403ca541c3b4f06adc
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-03-21 10:32:36 -0300

    feat: create new method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        App p = new App();
+        int number = (int) Math.floor(Math.random() * 10);
+        p.pyramidOfNumber(number);
+    }
+
+    void pyramidOfNumber(int number) {
+        System.out.println("functional");
     }
 }
